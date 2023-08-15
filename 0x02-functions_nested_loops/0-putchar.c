a6ac60123f9fc681a2a2f53a6382e40b45426d9e
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-15 16:28:51 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i
+	char word[8]="_putchar";
+	for (i=0,i<9,i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+	return(0);
+}
