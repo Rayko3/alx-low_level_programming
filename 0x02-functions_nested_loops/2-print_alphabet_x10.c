9bab1eb69def82ae28bf71a6f7f6468b61fe831c
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-15 17:52:14 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet in lowercase 10 times
+ *
+ * Return: void.
+ */
+
+void print_alphabeti_x10(void)
+{
+        int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+        {
+                _putchar(i);
+        }
+        _putchar('\n');
+}
