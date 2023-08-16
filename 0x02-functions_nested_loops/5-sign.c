6fb712c03897837ab46b309fa28d599455d556bf
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-15 18:56:40 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - check the sign of an int
+ *
+ *
+ *@n : is an int
+ *
+ *
+ * Return: 1 if positive , 0 nul , -1 if negative
+ *
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
