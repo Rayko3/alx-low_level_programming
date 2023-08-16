7c349d3f7128b20e5be3ebbdc6c234a901bbccb6
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-15 18:13:19 -0700

    task2 after betty checking

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-        int i;
+	int i;
 	int j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 97; i <= 122; i++)
-        {
-                _putchar(i);
-        }
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
-        _putchar('\n');
 }
