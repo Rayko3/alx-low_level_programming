b132e608d00f12c0d4ce352a8a61f0c8bed8e28f
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-15 18:39:37 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if char is an alphabetic char
+ *
+ *
+ *@c : is a ASCII character
+ *
+ *
+ * Return: 1 if true , 0 if not
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 122 && c >= 97 || c <= 90 && c >= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
