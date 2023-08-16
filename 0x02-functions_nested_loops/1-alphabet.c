5358f543ce1d51db96b281034706918fd11521c7
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-15 17:14:37 -0700

    task2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		int i;
+
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		return(void);
+	}
+	return(0)
+}			
