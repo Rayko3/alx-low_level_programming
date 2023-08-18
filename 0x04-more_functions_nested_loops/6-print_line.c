cbd7351bfed7cf38849d72868196f485fab89aba
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-17 17:18:07 -0700

    till task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_line - draws a straight line in the terminal
+ *
+ *@n : an integer
+ *
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n ; i++)
+		{
+			_putchat('_');
+		}
+	}
+	_putchar('\n');
+}
