d90b8b91b9c37502eda7ad23fc2c0d3ae7bd51cb
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-21 13:15:00 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @*s : pointer to a char
+ * @s : is a char
+ * Return: returns the lenght of the string
+ */
+
+
+int _strlen(char *s)
+{
+	return (sizeof(*s));
+}
