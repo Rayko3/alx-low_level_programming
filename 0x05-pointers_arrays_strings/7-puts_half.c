56a127026cd0cca71afc5779b05e3713821e78d5
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-21 18:12:31 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *puts_half - prints half of a string
+ *@str : pointer to a string
+ */
+
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		for (j = (i / 2) + 1; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
