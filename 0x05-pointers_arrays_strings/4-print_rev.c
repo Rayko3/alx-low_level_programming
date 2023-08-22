5b0dd27fc210b581fadb3f5afd117530378a92f1
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-21 17:20:52 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- *_puts -  prints a string in reverse
- *@str : pointer to a string
+ *print_rev -  prints a string in reverse
+ *@s : pointer to a string
  */
 
 
-void print_rev(char *str)
+void print_rev(char *s)
 {
-        int i = 0;
+	int i = 0;
 	int j;
 
-        while (str[i] != '\0')
-        {
-                i++;
-        }
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	for (j = 0; j < i; j++)
 	{
-		_putchar(s[i-j-1]);
+		_putchar(s[i - j - 1]);
 	}
 	_putchar('\n');
 }
