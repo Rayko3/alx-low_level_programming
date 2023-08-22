9d782ed2589f1837ea55c70816aebe66b4c47c58
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-21 18:24:43 -0700

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d,%c", a[i], ' ');
 	}
-	printf("\\n");
+	printf("%d\n", a[n - 1]);
 }
