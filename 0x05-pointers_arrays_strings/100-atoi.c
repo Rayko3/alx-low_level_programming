06cb16ccde127639feb5a62cea8eca31417b5c36
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-21 19:20:10 -0700

    task10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *_atoi - convert a string to an integer
+ *@s : pointer to a string
+ *Return: the integer
+ */
+
+int _atoi(char *s)
+{
+	int k = 1;
+	int i = 0;
+	int j;
+	int l = 0;
+	int p = 0;
+	int numb = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] == 45)
+		{
+			k = k * (-1);
+		}
+	}
+	while ((s[l] < 48 || s[l] > 57) && (l <= i))
+	{
+		l++;
+	}
+	if ((s[l] < 48 || s[l] > 57) && (l == i))
+	{
+		return (0);
+	}
+	while (s[l] >= 48 && s[l] <= 57)
+	{
+		numb = numb + (s[l] * (10 * *p));
+		l++;
+		p++;
+	}
+	return (numb);
+}
