353fad852f5c1237ca2a773a8f5eeaf7d0f58c0c
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-24 17:45:08 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *@s1 : pointer to a string
+ *@s2 : pointer to a string
+ *Return: an integer
+ */
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	if (i > j)
+	{
+		return (15);
+	}
+	else if (j > i)
+	{
+		return (-15);
+	}
+	else
+	{
+		return (0);
+	}
+}
