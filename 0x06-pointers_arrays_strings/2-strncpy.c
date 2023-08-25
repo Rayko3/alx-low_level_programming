05d2b162ae61ca171192c760cf6a7b0dfcc94142
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-24 17:15:25 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strncp - coppy a string up to n bytes
+ **@dest : pointer to a string
+ *@src : pointer to a string
+ *@n : an integer
+ *Return: pointer to a string
+ */
+
+char *_strncp(char *dest, char *src, int n)
+{
+	int j = 0;
+
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	if (j < n && src[j] == '\0')
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
