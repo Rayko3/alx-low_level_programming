f70b24f59de3d5c4c5cc7eb47eba51b39cde81f1
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-24 18:19:11 -0700

    task3 understading what it mean to compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	return (i - j);
+	return (0);
 }
