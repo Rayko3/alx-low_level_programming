a6cbd0d85e89a2ddde550d74dca8736dc1cf8f82
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-24 17:33:04 -0700

    forgot the null bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,6 +11,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
+	int i;
 
 
 	while (src[j] != '\0' && j < n)
@@ -18,6 +19,9 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = src[j];
 		j++;
 	}
-	dest[j] = '\0';
+	for (i = j; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
