baeb2aeda578728f59b1d9aa3e4ad6622fd0aaeb
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-08-28 17:07:07 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ *_memset -  fills memory with a constant byte
+ *@s : pointer to a string
+ *@b : char being filled
+ *@n : number of bytes to be changed
+ *Return: pointer to a string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
