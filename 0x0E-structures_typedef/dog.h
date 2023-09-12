82d512923ed478f9c5a12d6615fc75bb8bc10ade
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-09-11 19:05:37 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,12 @@
  *@owner : pointer to the owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
+}dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
