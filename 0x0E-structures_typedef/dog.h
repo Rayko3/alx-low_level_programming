43e63e6d22e065e5566b775417ce9133316986a1
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-09-11 18:14:28 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - struc about dog's informations
+ *@name : pointer to the name of the dog
+ *@age : pointer to the age of the dog
+ *@owner : pointer to the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
