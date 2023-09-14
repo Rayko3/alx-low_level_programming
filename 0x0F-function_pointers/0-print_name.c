077d5754806bfc1b710ba0810eeaccc7a8e68cac
AIT BELAID <youssefaitbelaid00@gmail.com>
2023-09-13 17:04:27 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "function_pointers.h"
 
 /**
  * print_name - Prints a name.
@@ -8,8 +8,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
 	f(name);
 }
